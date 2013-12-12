3ee6603c6ddefb2773e41b90af78d253880f0b5c
SungHyunJun <hjsung@gmail.com>
2013-12-12 19:41:56 +0900

    update parse setting

--- ChatCheck/ChatCheck/AppDelegate.m
+++ ChatCheck/ChatCheck/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse.h>
 
 @implementation AppDelegate
 
@@ -16,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [Parse setApplicationId:@"kNjU450yOhRnxgSCQCNvlA1MmomcW4wZpBDtlV1r"
+                  clientKey:@"ZL3pEMGDE7qVNWUe662DbsfuJ7VqfIfzbzAz2l2O"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+
     return YES;
 }
 
