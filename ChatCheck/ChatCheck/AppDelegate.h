01db73c9d24528708564a686282689d5db7c85df
SungHyunJun <hjsung@gmail.com>
2013-12-12 16:02:22 +0900

    project initial setting

---
+++ ChatCheck/ChatCheck/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ChatCheck
+//
+//  Created by hjazz on 12/12/13.
+//  Copyright (c) 2013 HJazz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
